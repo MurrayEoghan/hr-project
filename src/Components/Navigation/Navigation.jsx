aa11e164d816c46f4c8ee5367db091e6a53f4928
MurrayEoghan <eoghanmurray69@gmail.com>
2024-06-11 19:45:45 -0700

    Updated nav styling

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-import {Container, Nav, Navbar, Button} from 'react-bootstrap'
+import {Container, Nav, Navbar} from 'react-bootstrap'
+import { Dropdown, Space, Menu } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
 import './style.scss'
 
 
 export default function Navigation() {
+
+    const items = [{
+        key: 1,
+        label: 'Messages',
+    }, {
+        key: 2,
+        label: 'Pending Requests'
+    }, {
+        key: 3,
+        label: 'Status Centre'
+    }, {
+        key: 4,
+        label: 'Log Out'
+    }]
+
+    // const items = (
+    //     <Menu>
+    //         <Menu.Item>Messages</Menu.Item>
+    //         <Menu.Item>Pending Requests</Menu.Item>
+    //         <Menu.Item>Status Centre</Menu.Item>
+    //     </Menu>
+    // )
+
     return (
         <Navbar className="nav-wrapper">
         <Container>
@@ -14,9 +39,9 @@ export default function Navigation() {
             <Nav.Link className='navigation-link'>Applications</Nav.Link>
           </div>
           <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>
-              <Button variant='success'>Mark Otto</Button>
-            </Navbar.Text>
+            <Dropdown menu={{items}} className='user-dropdown'>
+                <Space><UserOutlined />Username</Space>
+            </Dropdown>
           </Navbar.Collapse>
         </Container>
       </Navbar>
