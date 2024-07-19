42f8b3abe44ff7c2cd309a1051705ca81c0a0df0
MurrayEoghan <eoghanmurray69@gmail.com>
2024-07-18 20:50:20 -0700

    Generic job posting example

--- src/Components/PostDisplayDrawer/PostDisplayDrawer.tsx
+++ src/Components/PostDisplayDrawer/PostDisplayDrawer.tsx
@@ -1,6 +1,15 @@
-import React, {useState} from 'react'
-import {Drawer} from 'antd'
-import type {DrawerProps} from 'antd'
+import React from 'react'
+import {Drawer, Space, Divider, Typography} from 'antd'
+import {  
+    CardMeta,
+    CardHeader,
+    CardDescription,
+    CardContent,
+    Card,
+    Image
+} from 'semantic-ui-react'
+
+import steve from '../../Assets/Images/steve.jpg'
 
 interface PostDisplayProps {
     title: string,
@@ -10,8 +19,36 @@ interface PostDisplayProps {
     isOpen: boolean
 }
 
+const {Title} = Typography
+
 export default function PostDisplayDrawer(props: PostDisplayProps) {
-    return <Drawer title={props.title} size={'large'} placement='right' open={props.isOpen} onClose={props.closeDrawer}>
-        {props.body}
+    return <Drawer size={'large'} placement='right' open={props.isOpen} onClose={props.closeDrawer}>
+        <Space direction='vertical' size='large' style={{display: 'flex'}}>
+            <Title level={2} style={{textAlign: 'center'}}>{props.title}</Title>
+            <Divider>Job Poster</Divider>
+            <Card
+                link
+                header='Eoghan Murray'
+                meta='Member since 2014'
+                description='Senior HR Recruiter @ ProjectRec'
+                fluid
+            >
+            <CardContent>
+                <Image
+                    floated='right'
+                    size='mini'
+                    circular
+                    src={steve}
+                />
+                <CardHeader>Eoghan Murray</CardHeader>
+                <CardMeta>Member since 2014</CardMeta>
+                <CardDescription>
+                    Senior HR Recruiter @ ProjectRec
+                </CardDescription>
+            </CardContent>
+            </Card>
+            <Divider>Job Description</Divider>
+            <span>{props.body}</span>
+        </Space>
     </Drawer>
 }
